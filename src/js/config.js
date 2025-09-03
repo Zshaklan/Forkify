ca8b95a33b88fbe1dbac59456f19323b433fd856
Zeevesh Shaklan <happyshaklan10499@gmail.com>
2025-09-03 23:36:23 +0530

    Fix icons path for Netlify

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
-export const TIMEOUT_SEC = 10;
+export const TIMEOUT_SEC = 30;
 export const MODAL_CLOSE_SEC = 2.5;
 export const RES_PER_PAGE = 10;
 export const KEY = '66602666-f3eb-4b89-a406-2ad4d83b3dd0';
